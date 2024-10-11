b670c77d0c2f7d76a10047ef87c4947490067301
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2024-10-11 14:07:15 -0600

    Created Products class which will create and store an array of products to be used by both the TagElementGenerator and SummaryTableGenerator based on parsed CSV data

---
+++ src/scripts/models/Products.js
@@ -0,0 +1,48 @@
+import Product from "./Product.js";
+
+/**
+ * Contains a list of Products and metadata regarding the state of said products
+ */
+export default class Products {
+	constructor() {
+		this.parsedData = null;
+		this.toArray = [Product];
+		this.isEmpty = this.toArray.length === 0;
+		this.isCategorized = false;
+		this.isAlphabetized = false;
+	}
+
+	setParsedData(parsedData) {
+		this.parsedData = parsedData;
+	}
+
+	/**
+	 * Accepts parsed data from a purchase order and stores it in an array as a local property.
+	 * @param {string} parsedData A string of parsed data
+	 */
+	createProducts() {
+		if (this.parsedData) {
+			const products = [];
+			parsedData = parsedData.data.slice(15);
+			for (let i = 0; i < parsedData.length; i++) {
+				const item = parsedData[i];
+				// PapaParse adds entries for blank spaces so we're not going to include those
+				if (item[0] !== "") {
+					const product = new Product(
+						item[0], // SKU
+						item[1], // Name
+						item[2], // Package Reference
+						item[3], // Category
+						item[4], // Subcategory
+						item[5], // Ordered Quantity
+						item[6], // Current Quantity
+						item[7], // Unit Cost
+						item[8] // Total Cost
+					);
+					products.push(product);
+				}
+			}
+			this.toArray = products;
+		}
+	}
+}
