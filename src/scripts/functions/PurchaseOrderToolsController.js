16775bb5ad5884d1806e038793c7fbc00ce380d2
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2024-10-24 16:10:40 -0600

    Products objects are now created when generation occurs. Added delegation to the generators, simplifying calls within the controller.

--- src/scripts/functions/PurchaseOrderToolsController.js
+++ src/scripts/functions/PurchaseOrderToolsController.js
@@ -1,14 +1,20 @@
 import ExportedCSVParser from "./ExportedCSVParser.js";
 import SummaryTableGenerator from "./SummaryTableGenerator.js";
-import TagElementGenerator from "./TagElementGenerator.js";
+import TagElementContainerGenerator from "./TagElementContainerGenerator.js";
 import Products from "../models/Products.js";
 
 export default class PurchaseOrderToolsController {
 	constructor() {
 		this.exportedCSVParser = new ExportedCSVParser();
-		this.tagElementGenerator = new TagElementGenerator();
 		this.summaryTableGenerator = new SummaryTableGenerator();
-		this.products = new Products();
+		this.products = null;
+		this.tagElementContainer = null;
+		this.summaryTableContainer = null;
+
+		/**
+		 * An object which contains options that determine how the table and tag
+		 * generation is handled.
+		 */
 		this.options = {
 			withBarcodes: false,
 			withSummary: false,
@@ -23,23 +29,53 @@ export default class PurchaseOrderToolsController {
 		this.options = options;
 	}
 
+	async generate(files) {
+		try {
+			await this.createProducts(files);
+			this.createTagElementContainer();
+			if (this.options.withSummary) {
+				this.createSummaryTable();
+			}
+		} catch (error) {
+			console.error(error.message);
+		}
+	}
+
 	async createProducts(files) {
 		this.exportedCSVParser.setFileList(files);
 		const parsedData = await this.exportedCSVParser.parse();
 		if (parsedData) {
-			this.products.setParsedData(parsedData);
+			this.products = new Products(parsedData, this.options.productOptions);
 			this.products.createProducts();
 		} else {
+			throw new Error("No parsed data to create products from.");
+			// console.error("No parsed data to create products from.");
 		}
 	}
 
-	async getTagElementContainer(files) {
-		await this.createProducts(files);
-		return this.tagElementGenerator.createTagElementContainer(
-			this.products.toArray,
+	createTagElementContainer() {
+		// instantiate new tag element generator with data
+		this.tagElementGenerator = new TagElementContainerGenerator(
+			this.products,
 			this.options.withBarcodes
+		);
+		// create
+		this.tagElementGenerator.createTagElementContainer();
+		// return
+		return this.tagElementGenerator.getTagElementContainer();
+	}
+
+	getTagElementContainer() {
+		return this.tagElementGenerator.getTagElementContainer();
+	}
+
+	createSummaryTable() {
+		this.summaryTableContainer = this.summaryTableGenerator.createSummaryTableContainer(
+			this.products.toArray
 		);
 	}
 
-	getSummaryTable() {}
+	getSummaryTable() {
+		return this.summaryTableContainer;
+	}
 }
