1cbfbd55a77dca9be74fef2e7b8bea3e0d6990fa
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2024-10-24 16:05:33 -0600

    started to create Summary Table Generator to create a table listing all products supplied which can be appended to the DOM

--- src/scripts/functions/SummaryTableGenerator.js
+++ src/scripts/functions/SummaryTableGenerator.js
@@ -18,17 +18,59 @@
  *      b. Optional SubCategory Sort - All In Ones/Disposables
  */
 export default class SummaryTableGenerator {
-	constructor(products, options) {
-		this.products = products;
-		if (options) {
-			this.options = options;
-		} else {
-			this.options = null;
-		}
-	}
+	constructor() {}
 
-	generate() {
-		// Build Header Titles
-		const headers = [];
+	createSummaryTableContainer(products) {
+		const summaryTableContainer = document.createElement("div");
+		summaryTableContainer.id = "summary-container";
+
+		const summaryTable = document.createElement("table");
+
+		// header
+		const summaryTableHeaderRow = document.createElement("tr");
+		const summaryTableHeaderSKU = document.createElement("th");
+		summaryTableHeaderSKU.textContent = "SKU";
+		const summaryTableHeaderProductName = document.createElement("th");
+		summaryTableHeaderProductName.textContent = "Product Name";
+		const summaryTableHeaderCategory = document.createElement("th");
+		summaryTableHeaderCategory.textContent = "Category";
+		const summaryTableHeaderQuantity = document.createElement("th");
+		summaryTableHeaderQuantity.textContent = "Quantity";
+
+		summaryTableHeaderRow.append(
+			summaryTableHeaderSKU,
+			summaryTableHeaderProductName,
+			summaryTableHeaderCategory,
+			summaryTableHeaderQuantity
+		);
+
+		summaryTable.appendChild(summaryTableHeaderRow);
+
+		// data
+		products.forEach((product) => {
+			const summaryTableDataRow = document.createElement("tr");
+			const summaryTableDataSKU = document.createElement("td");
+			summaryTableDataSKU.classList.add("td-sku");
+			summaryTableDataSKU.textContent = product.SKU;
+			const summaryTableDataProductName = document.createElement("td");
+			summaryTableDataProductName.classList.add("td-product-name");
+			summaryTableDataProductName.textContent = product.name.toString();
+			const summaryTableDataCategory = document.createElement("td");
+			summaryTableDataCategory.classList.add("td-category");
+			summaryTableDataCategory.textContent = product.category;
+			const summaryTableDataQuantity = document.createElement("td");
+			summaryTableDataQuantity.classList.add("td-quantity");
+			summaryTableDataQuantity.textContent = product.orderedQty;
+			summaryTableDataRow.append(
+				summaryTableDataSKU,
+				summaryTableDataProductName,
+				summaryTableDataCategory,
+				summaryTableDataQuantity
+			);
+			summaryTable.appendChild(summaryTableDataRow);
+		});
+
+		summaryTableContainer.appendChild(summaryTable);
+		return summaryTableContainer;
 	}
 }
