81da45aafead9a84c5513c9226dff035738560c5
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2024-10-24 19:41:05 -0600

    Implemented ability to generate multiple tables based on the product category

--- src/scripts/functions/SummaryTableGenerator.js
+++ src/scripts/functions/SummaryTableGenerator.js
@@ -23,17 +23,184 @@ export default class SummaryTableGenerator {
 		this.withCategoryTables = withCategoryTables;
 	}
 
-	createSummaryTableContainer(products) {
+	createSummaryTableContainer() {
 		const summaryTableContainer = document.createElement("div");
 		summaryTableContainer.id = "summary-container";
-		summaryTableContainer.appendChild(this.createSummaryTable(products));
+		if (this.withCategoryTables) {
+			summaryTableContainer.appendChild(this.createCategoryTables());
+		} else {
+			summaryTableContainer.appendChild(this.createSummaryTable());
+		}
 		return summaryTableContainer;
 	}
 
-	createSummaryTable(products) {
+	createCategoryTables() {
+		const categoryTableContainer = document.createElement("div");
+		categoryTableContainer.id = "category-table-container";
+
+		// beverages
+		categoryTableContainer.appendChild(
+			this.createCategoryTable("Beverages", this.products.sortedCategoryContainer.beverage)
+		);
+
+		// concentrates
+		categoryTableContainer.appendChild(
+			this.createCategoryTable(
+				"Concentrates",
+				this.products.sortedCategoryContainer.concentrate
+			)
+		);
+
+		// edibles
+		if (this.products.options.sortEdibles) {
+			// optional chocolate
+			if (this.products.sortedCategoryContainer.edible.chocolate.length > 0) {
+				categoryTableContainer.appendChild(
+					this.createCategoryTable(
+						"Chocolates",
+						this.products.sortedCategoryContainer.edible.chocolate
+					)
+				);
+			}
+
+			// optional gummy
+			if (this.products.sortedCategoryContainer.edible.gummy.length > 0) {
+				categoryTableContainer.appendChild(
+					this.createCategoryTable(
+						"Gummies",
+						this.products.sortedCategoryContainer.edible.gummy
+					)
+				);
+			}
+		} else {
+			// or all unsorted
+			categoryTableContainer.appendChild(
+				this.createCategoryTable(
+					"Edibles",
+					this.products.sortedCategoryContainer.edible.unsorted
+				)
+			);
+		}
+		// flower
+		//// eighth
+		categoryTableContainer.appendChild(
+			this.createCategoryTable("Eighths", this.products.sortedCategoryContainer.flower.eighth)
+		);
+		//// quarter
+		categoryTableContainer.appendChild(
+			this.createCategoryTable(
+				"Quarters",
+				this.products.sortedCategoryContainer.flower.quarter
+			)
+		);
+		//// half
+		categoryTableContainer.appendChild(
+			this.createCategoryTable("Halfs", this.products.sortedCategoryContainer.flower.half)
+		);
+		//// ounce
+		categoryTableContainer.appendChild(
+			this.createCategoryTable("Ounces", this.products.sortedCategoryContainer.flower.ounce)
+		);
+		// prerolls
+		//// non infused
+		categoryTableContainer.appendChild(
+			this.createCategoryTable(
+				"Non-Infused Pre-Rolls",
+				this.products.sortedCategoryContainer.preroll.nonInfused
+			)
+		);
+		//// infused
+		categoryTableContainer.appendChild(
+			this.createCategoryTable(
+				"Infused Pre-Rolls",
+				this.products.sortedCategoryContainer.preroll.infused
+			)
+		);
+		// capsules, oils and topicals
+		categoryTableContainer.appendChild(
+			this.createCategoryTable(
+				"Capsules, Oils and Topicals",
+				this.products.sortedCategoryContainer.capsuleOilTopical
+			)
+		);
+
+		// vaporizers
+		if (this.products.options.sortVaporizers) {
+			// optional cartridge
+			if (this.products.sortedCategoryContainer.vaporizer.cartridge.length > 0) {
+				categoryTableContainer.appendChild(
+					this.createCategoryTable(
+						"510 Cartridges",
+						this.products.sortedCategoryContainer.vaporizer.cartridge
+					)
+				);
+			}
+
+			// optional pods
+			if (this.products.sortedCategoryContainer.vaporizer.pod.length > 0) {
+				categoryTableContainer.appendChild(
+					this.createCategoryTable(
+						"Pods",
+						this.products.sortedCategoryContainer.vaporizer.pod
+					)
+				);
+			}
+
+			// optional disposable
+			if (this.products.sortedCategoryContainer.vaporizer.disposable.length > 0) {
+				categoryTableContainer.appendChild(
+					this.createCategoryTable(
+						"Disposables",
+						this.products.sortedCategoryContainer.vaporizer.disposable
+					)
+				);
+			}
+		} else {
+			// or all unsorted
+			categoryTableContainer.appendChild(
+				this.createCategoryTable(
+					"Vaporizers",
+					this.products.sortedCategoryContainer.vaporizer.unsorted
+				)
+			);
+		}
+
+		// all others
+		if (this.products.sortedCategoryContainer.other.length > 0) {
+			categoryTableContainer.appendChild(
+				this.createCategoryTable("Other", this.products.sortedCategoryContainer.other)
+			);
+		}
+
+		return categoryTableContainer;
+	}
+
+	createCategoryTable(categoryName, category) {
+		const summaryTable = document.createElement("table");
+		const tableCaption = document.createElement("caption");
+		tableCaption.textContent = categoryName;
+		summaryTable.appendChild(tableCaption);
+		summaryTable.appendChild(this.createCategoryTableHeader());
+		summaryTable.appendChild(this.createCategoryTableBody(category));
+		return summaryTable;
+	}
+
+	createCategoryTableHeader() {
+		return this.createSummaryTableHeader();
+	}
+
+	createCategoryTableBody(category) {
+		const categoryTableBody = document.createElement("tbody");
+		category.forEach((item) => {
+			categoryTableBody.appendChild(this.createSummaryTableRow(item));
+		});
+		return categoryTableBody;
+	}
+
+	createSummaryTable() {
 		const summaryTable = document.createElement("table");
 		summaryTable.appendChild(this.createSummaryTableHeader());
-		summaryTable.appendChild(this.createSummaryTableBody(products));
+		summaryTable.appendChild(this.createSummaryTableBody());
 		return summaryTable;
 	}
 
@@ -59,9 +226,9 @@ export default class SummaryTableGenerator {
 		return summaryTableHeader;
 	}
 
-	createSummaryTableBody(products) {
+	createSummaryTableBody() {
 		const summaryTableBody = document.createElement("tbody");
-		products.forEach((product) =>
+		this.products.toArray.forEach((product) =>
 			summaryTableBody.appendChild(this.createSummaryTableRow(product))
 		);
 		return summaryTableBody;
